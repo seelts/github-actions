ee6e935acc8fe9d4b2148c89ddf9d2b414bbdc56
Ilia Liachin <i.liachin@youngcapital.nl>
2022-04-08 00:29:05 +0200

    Create "Get repository version tags" action

---
+++ get-repository-version-tags/script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+git fetch --quiet origin 'refs/tags/*:refs/tags/*'
+git tag -l | jq --raw-input '.' | jq --slurp --compact-output '[.[] | select(test("^v[0-9]")) | ltrimstr("v")]'
